ea519cd0108f508444cb775d2855cdce0032dce4
harsha <uguy008@outlook.com>
2025-01-20 05:55:04 +0530

    Interrupts

--- OS-files/kernel.cpp
+++ OS-files/kernel.cpp
@@ -1,5 +1,6 @@
 #include "types.h"
 #include "gdt.h"
+#include "interrupts.h"
 
 void printf(char* str)
 {
@@ -62,6 +63,9 @@ extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber)
     printf("Demn Bwoi.. Next Video memory");
 
     GlobalDescriptorTable gdt;
+    InterruptManager interrupts(&gdt);
+
+    interrupts.Activate();
 
     while (1);
 }
