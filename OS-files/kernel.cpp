b5151bba13b803812cee5da2f8dd6dd7bd3d403c
harsha <uguy008@outlook.com>
2025-01-21 01:57:17 +0530

    GDT

---
+++ OS-files/kernel.cpp
@@ -0,0 +1,32 @@
+#include "types.h"
+#include "gdt.h"
+
+void printf(char* str)
+{
+    static uint16_t* VideoMemory = (uint16_t*)0xb8000;
+
+    for (int i = 0; str[i] != '\0'; ++i)
+        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
+}
+
+typedef void (*constructor)();
+extern "C" constructor start_ctors;
+extern "C" constructor end_ctors;
+extern "C" void callConstructors()
+{
+    for (constructor* i = &start_ctors; i != &end_ctors; i++)
+        (*i)();
+}
+
+extern "C" void __stack_chk_fail_local() {
+    while (1);
+}//to fix stack_chk_fail_local error fix
+
+extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber)
+{
+    printf("BINGO!!! Shit's booted fr...");
+
+    GlobalDescriptorTable gdt;
+
+    while (1);
+}
