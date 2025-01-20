de1c07976652b85968dab954adc6656d555f80fc
harsha <uguy008@outlook.com>
2025-01-20 06:51:25 +0530

    Interrupt fixes
    GDT

--- OS-files/kernel.cpp
+++ OS-files/kernel.cpp
@@ -1,45 +1,12 @@
 #include "types.h"
 #include "gdt.h"
-#include "interrupts.h"
 
 void printf(char* str)
 {
     static uint16_t* VideoMemory = (uint16_t*)0xb8000;
 
-    static uint8_t x =0 , y =0;
-
     for (int i = 0; str[i] != '\0'; ++i)
-    {
-
-            switch(str[i])
-            {
-                case '\n':
-                    y++;
-                    x = 0;
-                    break;
-                default:
-                    VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xFF00) | str[i];
-                    x++;
-                    break;
-            }
-
-            if(x >= 80)
-            {
-                y++;
-                x = 0;
-            }
-
-            if (y >= 25)
-            {
-                for(y = 0; y < 25; y++)
-                    for(x = 0; x < 80; x++)
-                        VideoMemory[80*y+x] = (VideoMemory[80*y+x] & 0xFF00) | ' ';
-                x = 0;
-                y = 0;
-            }
-
-    }
-
+        VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
 }
 
 typedef void (*constructor)();
@@ -51,21 +18,15 @@ extern "C" void callConstructors()
         (*i)();
 }
 
-
-//to fix stack_chk_fail_local error fix
 extern "C" void __stack_chk_fail_local() {
     while (1);
-}
+}//to fix stack_chk_fail_local error fix
 
 extern "C" void kernelMain(void* multiboot_structure, uint32_t magicnumber)
 {
-    printf("BINGO!!! Shit's booted fr...\n");
-    printf("Demn Bwoi.. Next Video memory");
+    printf("BINGO!!! Shit's booted fr...");
 
     GlobalDescriptorTable gdt;
-    InterruptManager interrupts(&gdt);
-
-    interrupts.Activate();
 
     while (1);
 }
