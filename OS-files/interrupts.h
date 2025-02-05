5f418532f5da2bbd61a681ba31d75910f7be0e9b
harsha <uguy008@outlook.com>
2025-02-05 18:18:44 +0530

    added interruptHandling for the timer and keyboard

--- OS-files/interrupts.h
+++ OS-files/interrupts.h
@@ -5,13 +5,30 @@
 
     class InterruptManager {
 
+
+        protected:
+            struct GateDescriptor
+            {
+                uint16_t handlerAddressLowBits;
+                uint16_t gdt_codeSegmentSelector;
+                uint8_t reserved;
+                uint8_t access;
+                uint16_t handlerAddressHighBits;
+                
+            } __attribute__((packed));
+
+            static GateDescriptor interruptDescriptorTable[256];
+
+
+
         public:
             static uint32_t handleInterrupt(uint8_t interruptNumber, uint32_t esp);
             
 
-
-
+            static void HandleInterruptRequest0x00();
+            static void HandleInterruptRequest0x01();
     };
+    
 
 
 #endif
