43ffc5bbd2a37fc17b8e79aa68f9bf1cddcfc7ab
harsha <uguy008@outlook.com>
2025-02-12 08:15:20 +0530

    mouse support added

--- OS-files/kernel.cpp
+++ OS-files/kernel.cpp
@@ -2,6 +2,7 @@
 #include "gdt.h"
 #include "interrupts.h"
 #include "keyboard.h"
+#include "mouse.h"
 
 
 void printf(char* str)
@@ -63,6 +64,7 @@ extern "C" void kernelMain(const void* multiboot_structure, uint32_t /*multiboot
     InterruptManager interrupts(0x20, &gdt);
 
     KeyboardDriver keyboard(&interrupts);
+    MouseDriver mouse(&interrupts);
     
     interrupts.Activate();
     
